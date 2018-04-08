d6f7934bb8f749c69933be2babc3e128dede3c44
eimepe <eimepe@yahoo.com>
2018-04-07 23:23:09 -0500

    update test

--- test.makerspro.com.co/dbConnect.php
+++ test.makerspro.com.co/dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(0);
+//error_reporting(0);
 define('DB_SERVER', 'mysqlcarwest.cdbsdrwiat5y.us-west-2.rds.amazonaws.com');
 define('DB_SERVER_USERNAME', 'eimepe');
 define('DB_SERVER_PASSWORD', 'eimepe73');
