992f0fb370673ef224c593cd8de83dcf9c94e95e
eimepe <eimepe@yahoo.com>
2018-04-08 02:13:56 -0500

    login update

--- login.php
+++ login.php
@@ -4,22 +4,22 @@
 require("config.php");
 
 $data = json_decode(file_get_contents('php://input'), true);
-
+$login_ok = false;
 
 
 if (!empty($data["usuario"])) {
     //obteneos los usuarios respecto a la usuario que llega por parametro
-    $query = " 
-            SELECT 
+    $query = "
+            SELECT
               *
-            FROM usuario 
-            WHERE 
+            FROM usuario
+            WHERE
                 codigo = :username and estado = 1";
-    
+
     $query_params = array(
         ':username' => $data['usuario']
     );
-    
+
     try {
         $stmt   = $db->prepare($query);
         $result = $stmt->execute($query_params);
@@ -27,40 +27,40 @@ if (!empty($data["usuario"])) {
     catch (PDOException $ex) {
         //para testear pueden utilizar lo de abajo
         //die("la consulta murio " . $ex->getMessage());
-        
+
         $response["success"] = 0;
         $response["message"] = "Problema con la base de datos, vuelve a intetarlo";
         die(json_encode($response));
-        
+
     }
-    
-    //la variable a continuación nos permitirará determinar 
+
+    //la variable a continuación nos permitirará determinar
     //si es o no la información correcta
     //la inicializamos en "false"
     $validated_info = false;
-    
+
     //bamos a buscar a todas las filas
     $row = $stmt->fetch();
-    
-    
+
+
     if ($row) {
         //si el password viene encryptado debemos desencryptarlo acá
         // ++ DESCRYPTAR ++//
 
         //encaso que no lo este, solo comparamos como acontinuación
         if ($data['clave'] === $row['clave']) {
-			
-			
+
+
 	$login_ok = true;
-           
-			
-			
-			
+
+
+
+
         }
     }
-    
+
     // así como nos logueamos en facebook, twitter etc!
-    // Otherwise, we display a login failed message and show the login form again 
+    // Otherwise, we display a login failed message and show the login form again
     if ($login_ok==true) {
         $response["success"] = 1;
 		$response["codigo"]=$row["codigo"];
@@ -76,18 +76,18 @@ if (!empty($data["usuario"])) {
     }
 } else {
 ?>
-  <h1>Login</h1> 
-  <form action="login.php" method="post"> 
-      Username:<br /> 
-      <input type="text" name="usuario" placeholder="username" /> 
-      <br /><br /> 
-      Password:<br /> 
-      <input type="password" name="clave" placeholder="password" value="" /> 
-      <br /><br /> 
-      <input type="submit" value="Login" /> 
-  </form> 
+  <h1>Login</h1>
+  <form action="login.php" method="post">
+      Username:<br />
+      <input type="text" name="usuario" placeholder="username" />
+      <br /><br />
+      Password:<br />
+      <input type="password" name="clave" placeholder="password" value="" />
+      <br /><br />
+      <input type="submit" value="Login" />
+  </form>
   <a href="register.php">Register</a>
  <?php
 }
 
-?> 
+?>
